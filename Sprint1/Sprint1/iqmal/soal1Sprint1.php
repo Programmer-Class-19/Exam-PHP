e4109b84c0ae6c12b8f9ee4cb10260fca475e88d
iqmalfarid8 <iqmalfarid8@gmail.com>
2024-08-12 11:27:08 +0700

    ujianyambahan

---
+++ Sprint1/Sprint1/iqmal/soal1Sprint1.php
@@ -0,0 +1,45 @@
+// 1
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ujian1</title>
+</head>
+<style>
+
+<?php
+$umur = 19;
+$nama2 = "farid";
+$buah = [["apel"], 
+         ["pisang"], 
+         ["alpukat"]];
+
+$datadiri = {
+    "$nama1": "farid iqmal hafizd";
+    "$umur": 18;
+    "$kota": "bogor";
+
+        echo = "$umur", "$heightheight", "$nama", "$buah", "$datadiri"
+
+};
+?>
+
+</style>
+<body>
+    
+</body>
+</html>
+
+// 2
+<?php
+echo "Hello World!";
+?>
+
+// 3
+<?php
+$x = 20
+$y = 30
+echo $x + $Y; 
+
+?>
