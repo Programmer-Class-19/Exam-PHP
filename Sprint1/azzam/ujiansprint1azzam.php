8e7f940c003abeb350071c99fd1cbc56bd13a0d7
Elhaqq06 <azzamhaqqi13@gmail.com>
2024-08-12 11:11:34 +0700

    nambah branch azzam

---
+++ Sprint1/azzam/ujiansprint1azzam.php
@@ -0,0 +1,24 @@
+<?php
+$var1 = "satu";
+$var2 = "dua";
+$var3 = "tiga";
+$var4 = "empat";
+?>
+<br>
+<?php
+$x = 50;
+$y = 30; 
+var_dump($x > 50 && $x % 2 == 0);
+var_dump($x < 50 || $x % 2 == 0);
+var_dump($y = 30 && $y % 2 == 0);
+var_dump($y > 30 || $y % 2 == 1);
+echo "<br>";
+
+
+echo $x + $y;
+echo "<br>";
+$x = 50;
+$x += 30;
+echo $x;
+
+?>
