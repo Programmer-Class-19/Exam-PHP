04d1047cae2f5cd14c9e4e46ce21d8c54bf8767e
deandra485 <deandraputrawiguna@gmail.com>
2024-08-19 23:05:09 +0700

    menambahkan session

--- Sprint1/deandra/jawaban soal.php
+++ Sprint1/deandra/jawaban soal.php
@@ -14,9 +14,23 @@ var_dump ( $x < 10 && $x % 5 == 0); // hasil nilai false
 var_dump ( $x < 50 || $x % 2 == 2); // hasil nilai true
 var_dump ( $x < 5 || $x % 5 == 4); // hasil nilai false
 
+// xor dan mot gk bisa jawab
 
+// aritmatika
+$x = 10;
+$y = 20;
+echo $x + $y; // 10 + 20 = 30
 
+// assignment
+$sngka = 50;
+$angka *= 10;
+echo $angka; // 50 * 10 = 500
 
+// perbandingan
+var_dump ( 1 < "10");
+
+// identitas
+var_dump ( 4 === "4");
 
 
 ?>
